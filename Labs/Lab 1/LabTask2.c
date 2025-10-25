744d7b3957f64fdd714350aa7ef4a34af8c90068
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-10-25 20:12:35 +0500

    Programming

---
+++ Labs/Lab 1/LabTask2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name:\tRabeet\nDepartment:\tCSIT\n");
+    printf("1st semester\n");
+}
---
+++ Labs/Lab 1/LabTask3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    int age = 18;
+    float marks = 91;
+    char grade = 'A';
+
+    printf("Age : %d\nMarks : %.2f\nGrade : %c\n",age,marks,grade);
+}
