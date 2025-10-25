00bc2d384c5cc58aea3e18e15f31487566e3094b
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-10-25 20:17:51 +0500

    Programming

---
+++ Labs/Lab 2/LabTask3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+	int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+
+   if(num>0)
+   {
+    printf("Positive number");
+   }
+   else if(num<0)
+   {
+   printf("negative number");
+   }
+   else
+   {
+    printf("Zero");
+   }
+
+}
