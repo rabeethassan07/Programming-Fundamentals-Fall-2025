4a890c613edd0758767441751ca5926cbc92c8b4
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-12 20:45:50 +0500

    C Programming

---
+++ Code In Place/Comparing Strings.c
@@ -0,0 +1,19 @@
+// Comparing Strings
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[50], str2[50];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if (strcmp(str1, str2) == 0)
+        printf("Equal\n");
+    else
+        printf("Not Equal\n");
+        
+}
