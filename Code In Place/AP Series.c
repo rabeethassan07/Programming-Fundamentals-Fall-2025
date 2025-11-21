c051123d9f8ef6518d3cb5d9da1e9482d145fc66
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-21 21:36:50 +0500

    C Programming

---
+++ Code In Place/AP Series.c
@@ -0,0 +1,12 @@
+// Display the AP-4,7,10,13,16 upto nth term*
+#include<stdio.h>
+int main ()
+{
+    int n;
+    
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    for(int i=4;i<=3*n+1;i=i+3)
+    printf("%d ",i);
+}
