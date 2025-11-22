ca27149f33c8a6b72514371ee027e63ebeff157e
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-22 21:26:03 +0500

    C Programming

---
+++ Code In Place/To Find the Size of an Array.c
@@ -0,0 +1,8 @@
+// To Find the Size of an Array
+#include<stdio.h>
+int main()
+{
+    int a[]={2,5,73,77,44,74,98,35,47,8,5,83,24,64,99,67,2234,13456,57,54,345,98,340,67,99,230};
+    
+    printf("%d",sizeof(a)/sizeof(a[0]));
+}
