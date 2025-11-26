828a8a0e8c4836df07930150c3c5142876b876af
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-26 22:36:52 +0500

    C Programming

---
+++ Code In Place/Display pairs whose sum is even (1–n range).c
@@ -0,0 +1,19 @@
+// Display pairs whose sum is even (1–n range)
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter Number: ");
+	scanf("%d",&num);
+	
+	for(int i=1; i<=num; i++)
+	{
+		for(int j=1;j<=num;j++)
+		{
+			if((i+j) %2 == 0)
+			{
+				printf("(%d,%d)",i,j);
+			}
+		}
+	}
+}
