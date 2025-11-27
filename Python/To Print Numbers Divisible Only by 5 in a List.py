d7f33600ece79bcc8713522206515f4a5e259e5d
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-27 23:32:22 +0500

    Python Programming

---
+++ Python/To Print Numbers Divisible Only by 5 in a List.py
@@ -0,0 +1,10 @@
+# To Print Numbers Divisible Only by 5 in a List
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for num in numbers:
+    if num > 500:
+        break
+    if num > 150:
+        continue
+    if num % 5 == 0:
+        print(num)
