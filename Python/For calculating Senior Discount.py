1be8fec791ed1c60e9ce13488c97d4245502d474
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-11-28 23:23:55 +0500

    Python Programming

---
+++ Python/For calculating Senior Discount.py
@@ -0,0 +1,10 @@
+# For calculating Senior Discount
+age = 50
+is_member = False
+if age >= 60:
+ if is_member:
+   print("30% senior discount!")
+ else:
+   print("20% senior discount.")
+else:
+  print("Not eligible for a senior discount.")
