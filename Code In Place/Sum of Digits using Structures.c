444ddb6a79cb76bb08ff7b60493f0889cc544256
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-01 21:47:32 +0500

    C Programming

---
+++ Code In Place/Sum of Digits using Structures.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int sum_of_digits(int n)
+{
+    int sum = 0;
+    while (n > 0)
+	{
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Sum of digits = %d\n", sum_of_digits(num));
+
+    return 0;
+}
