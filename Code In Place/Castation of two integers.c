4d5c63f63b2e7c09ffd15a4b92e871946be07e92
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-07 22:01:30 +0500

    C Programming

---
+++ Code In Place/Castation of two integers.c
@@ -0,0 +1,14 @@
+// Castation of two integers
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    float b;
+
+    b = (float)a;
+
+    printf("Int value: %d\n", a);
+    printf("Float value after casting: %.2f\n", b);
+
+    return 0;
+}
