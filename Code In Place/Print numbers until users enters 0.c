45d784c9326c2832d019b2ff789420bf64b16523
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-12 21:34:34 +0500

    C Programming

---
+++ Code In Place/Print numbers until users enters 0.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// print the numbers until users enters 0
+int main()
+{
+    for (int i = 1; i != 0; i--)
+	{
+        printf("%d\n", i);
+        scanf("%d", &i);
+        i++;
+    }
+    printf("Terminating...\n");
+    return 0;
+}
