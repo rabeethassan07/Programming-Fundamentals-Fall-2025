ec681c8d555f648fb5d52cfc66fe8f131b7fd6ab
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-16 23:08:15 +0500

    C Programming

---
+++ Code In Place/A simple Logic of Roman to Int.c
@@ -0,0 +1,34 @@
+// Problem Link: https://leetcode.com/problems/roman-to-integer/
+
+int val(char sym)
+{
+    switch(sym)
+	{
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s)
+{
+    int result = 0, current, next;
+    for (int i = 0; s[i]!='\0'; i++)
+	{
+        current = val(s[i]);
+        next = val(s[i+1]);
+        
+        if (current < next)
+		{
+            result -= current;
+        }
+		else
+            result += current;
+    }
+    return result;
+}
