58bb03ea16ae27cef69c82a2bd9d5bec5dcface5
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-18 21:40:26 +0500

    C Programming

---
+++ Code In Place/Finding Largest and Smallest in a Array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+	{
+        scanf("%d", &arr[i]);
+    }
+
+    int smallest = 0;
+    int largest = 0;
+
+    for(i = 1; i < n; i++)
+	{
+        if(arr[i] < smallest)
+            smallest = arr[i];
+
+        if(arr[i] > largest)
+            largest = arr[i];
+    }
+
+    printf("\nSmallest element = %d\n", smallest);
+    printf("Largest element  = %d\n", largest);
+
+    return 0;
+}
