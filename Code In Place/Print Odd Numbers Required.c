d5549262228ee57ff80499c24971d832e4fc2f96
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-19 23:42:50 +0500

    C Programming

---
+++ Code In Place/Print Odd Numbers Required.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+
+    for(int i = 0; i < n; i++)
+	{
+        printf("%d ", 2*i + 1);
+    }
+    return 0;
+}
+
