6faf6adbcf9a33f6e51878b66a6894e8175c0e92
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2025-12-19 23:44:47 +0500

    C Programming

---
+++ Code In Place/Print Even Numbers Required.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 2; i <= 2 * n; i += 2)
+	{
+        printf("%d ", i);
+    }
+    return 0;
+}
